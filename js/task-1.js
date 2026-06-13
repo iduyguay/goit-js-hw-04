04b07c20037b6be0cb5b6d2ed60eb48dcea343c2
iduyguay <iduyguaykas@gmail.com>
2026-06-13 19:39:06 +0300

    İlk commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function isEnooghCapaciy(products, containerSize) {
+  const quantities = Object.values(products);
+  let totalQuantity = 0;
+  for (const quantity of quantities) {
+    totalQuantity += quantity;
+  }
+  return totalQuantity <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
